3201e879524a0f90621e2f94369943dc58ab7474
De Almeida Tiago <116738129+Tiagz92@users.noreply.github.com>
2025-01-08 20:48:01 +0100

    Background and animation add

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,39 @@ import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import { distance } from 'framer-motion';
 
 const App: React.FC = () => {
+  const particlesInit = async (main: any) => {
+    await loadFull(main);
+  };
   return (
     <div className="app">
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: {
+          number: {
+            value: 50,
+          },
+          size: {
+            value: 3,
+          },
+          move: {
+            enable: true,
+            speed: 1,
+          },
+          links: {
+            enable: true,
+            color: "61dafb",
+            distance: 150,
+          },
+        },
+      }}
+    />
       <Hero />
       <Projects />
       <About />
